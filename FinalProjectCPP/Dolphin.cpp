5f0413960bf0faac9c8a989bd25753faf78ec443
Shmuel Malikov <sshmoel1@gmail.com>
2022-06-09 12:26:47 +0300

    V9

--- FinalProjectCPP/Dolphin.cpp
+++ FinalProjectCPP/Dolphin.cpp
@@ -4,6 +4,7 @@
 #include "Animal.h"
 #include "Water.h"
 #include "Dolphin.h"
+#include "Mammal.h"
 
 using namespace std;
 
@@ -12,9 +13,9 @@ Dolphin::Dolphin()
 	this->Jump_Height = 0;
 }
 
-Dolphin::Dolphin(char* name, float age, float Swim_Speed, float Jump_Height) :Water(name, age, Swim_Speed), Animal(name, age)
+Dolphin::Dolphin(char* name, float age, float Swim_Speed, char* foodtype, float jump_height) :Water(name, age, Swim_Speed), Animal(name, age), Mammal(name, age, Swim_Speed, foodtype)
 {
-	this->Jump_Height = Jump_Height;
+	this->Jump_Height = jump_height;
 }
 
 Dolphin::Dolphin(const Dolphin& d) : Water(d), Animal(d)
@@ -29,10 +30,6 @@ Dolphin::~Dolphin()
 void Dolphin::print() const
 {
 	Water::print();
-	cout << "The Animal is: Dolphin" << endl;
-	cout << "Name: " << name << endl;
-	cout << "Age: " << age << endl;
-	cout << "It's speed is: " << Swim_Speed << " Kmh." << endl;
-	cout << "It's jump height is: " << Jump_Height << " Meters." << endl;
-
+	Mammal::print();
+	cout << "The Dolphin jump height is: " << Jump_Height << "meters." << endl;
 }
--- FinalProjectCPP/Dolphin.h
+++ FinalProjectCPP/Dolphin.h
@@ -3,16 +3,17 @@
 #include <iostream>
 #include "Animal.h"
 #include "Water.h"
+#include "Mammal.h"
 
 using namespace std;
 
-class Dolphin :public Water
+class Dolphin : public Mammal
 {
 protected:
 	float Jump_Height;
 public:
 	Dolphin();//default ctor
-	Dolphin(char* name, float age, float Swim_Speed, char* foodtype, float Jump_Height);//ctor
+	Dolphin(char* name, float age, float Swim_Speed, char* foodtype, float jump_height);//ctor
 	Dolphin(const Dolphin& d);//copy ctor
 	virtual ~Dolphin();//dctor
 	virtual void print() const;//print func
