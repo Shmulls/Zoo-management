b216254ac036b4d87b9574365d70740507754508
nikatati <73596287+nikatati@users.noreply.github.com>
2022-06-09 11:01:32 +0300

    v4

--- FinalProjectCPP/Eagle.h
+++ FinalProjectCPP/Eagle.h
@@ -1,5 +1,18 @@
 #pragma once
-class Eagle
-{
-};
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Air.h"
+#include "Animal.h"
+using namespace std;
 
+class Eagle :public Air
+{
+protected:
+	float Max_Height;
+public:
+	Eagle();//default ctor
+	Eagle(char* name, float age, float Air_Speed, float Max_Height);//ctor
+	Eagle(const Eagle& E);//copy ctor
+	virtual ~Eagle() {}//dctor
+	virtual void print() const;//print func
+};
