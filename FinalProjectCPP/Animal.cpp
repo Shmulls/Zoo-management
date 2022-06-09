12c1f31f1211b3d6bacf4578ce68e6abd1db6c49
Shmuel Malikov <sshmoel1@gmail.com>
2022-06-09 10:37:29 +0300

    V2

--- FinalProjectCPP/Animal.cpp
+++ FinalProjectCPP/Animal.cpp
@@ -1,1 +1,41 @@
 #include "Animal.h"
+
+Animal::Animal()
+{
+	name = NULL;
+	age = 0.0;
+}
+
+Animal::Animal(char* name, float age)
+{
+	name = new char[strlen(name) + 1];
+	if (name)
+	{
+		strcpy(this->name, name);
+		this->age = age;
+	}
+	else
+	{
+		name = NULL;
+		cout << "Name ctor is NULL" << endl;
+	}
+}
+
+Animal::Animal(const Animal& A)
+{
+	name = new char[strlen(A.name) + 1];
+	if (name == NULL)
+	{
+		name = NULL;
+		cout << "Name is NULL" << endl;
+	}
+	strcpy(name, A.name);
+	age = A.age;
+	count++;
+}
+
+Animal::~Animal()
+{
+	delete[]name;
+	count--;
+}
